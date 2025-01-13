ffd68545343c87af2b05d66f083e032da7389d7c
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-13 14:50:06 +0700

    penambahan code InsertBodyMhs pada InsertView.kt

--- app/src/main/java/com/example/praktikum15/ui/view/InsertView.kt
+++ app/src/main/java/com/example/praktikum15/ui/view/InsertView.kt
@@ -1,9 +1,13 @@
 package com.example.praktikum15.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -14,11 +18,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum15.ui.viewmodel.FormState
+import com.example.praktikum15.ui.viewmodel.InsertUiState
 import com.example.praktikum15.ui.viewmodel.InsertViewModel
+import com.example.praktikum15.ui.viewmodel.MahasiswaEvent
 import com.example.praktikum15.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -87,6 +95,44 @@ fun InsertMhsView(
                     }
                 }
             )
+        }
+        @Composable
+        fun InsertBodyMhs(
+            modifier: Modifier = Modifier,
+            onValueChange: (MahasiswaEvent) -> Unit,
+            uiState: InsertUiState,
+            onClick: () -> Unit,
+            homeUiState: FormState
+        ){
+            Column(
+                modifier = modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                FormMahasiswa(
+                    mahasiswaEvent = uiState.insertUiEvent,
+                    onValueChange = onValueChange,
+                    errorState = uiState.isEntryValid,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    onClick = onClick,
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = homeUiState !is FormState.Loading,
+                ) {
+                    if (homeUiState is FormState.Loading){
+                        CircularProgressIndicator(
+                            color = Color.White,
+                            modifier = Modifier
+                                .size(20.dp)
+                                .padding(end = 8.dp)
+                        )
+                        Text("Loading...")
+                    }else{
+                        Text("Add")
+                    }
+                }
+            }
         }
     }
 }
