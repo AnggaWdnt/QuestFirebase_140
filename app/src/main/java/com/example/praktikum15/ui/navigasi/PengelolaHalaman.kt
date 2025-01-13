e33467918a1a2dfa66d71e352420db3a967dc184
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-13 15:05:03 +0700

    penambahan code PengelolaHalaman.kt pada package navigasi

--- app/src/main/java/com/example/praktikum15/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum15/ui/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,37 @@
 package com.example.praktikum15.ui.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum15.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeView(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
