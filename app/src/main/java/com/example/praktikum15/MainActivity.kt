1b3462b73291520183a0ea6199593908b23f62fc
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-13 14:33:33 +0700

    penambahan code clas InsertViewModel.kt

--- app/src/main/java/com/example/praktikum15/MainActivity.kt
+++ app/src/main/java/com/example/praktikum15/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.praktikum15.ui.pages.HomeScreen
+import com.example.praktikum15.ui.view.HomeScreen
 import com.example.praktikum15.ui.theme.Praktikum15Theme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/praktikum15/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum15/ui/viewmodel/InsertViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.praktikum15.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.praktikum15.repository.Repositorymhs
+
+class InsertViewModel (
+    private val repositorymhs: Repositorymhs
+): ViewModel(){
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent){
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
